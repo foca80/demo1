3f96df6d7b5f8cdc4205feb060c89997d57d182b
flores.orihuela <flores.orihuela@gmail.com>
2020-07-01 23:46:24 -0500

    04.00

--- src/main/java/pe/edu/upc/SpringSecurityConfig.java
+++ src/main/java/pe/edu/upc/SpringSecurityConfig.java
@@ -22,6 +22,9 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		http.requiresChannel()
+	      .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
+	      .requiresSecure();
 
 		http.authorizeRequests().antMatchers("/", "/css/**", "/js/**", "/img/**").permitAll().anyRequest()
 				.authenticated().and().formLogin().loginPage("/login").permitAll().and().logout().permitAll().and()
